7c0759ff3855028a4f5c8bd9e68e6d80604277c8
Marcos <marcos.schneider13@hotmaill.com>
2019-04-27 18:29:39 -0300

    Adicionado api para poder fazer login

--- app/controller/api.js
+++ app/controller/api.js
@@ -3,17 +3,75 @@ var express = require('express');
 var router 	= express.Router();
 var Control = require('./control.js');
 var control = new Control;
+var apiModel = require('../model/apiModel.js');
+var model = new apiModel;
 var data = {};
 var app = express();
 app.use(require('express-is-ajax-request'));
 
 /* GET pagina de login. */
 router.get('/', function(req, res, next) {
+	res.json(0);
+});
 
-    if (req.isAjaxRequest())
-        res.send('request made through ajax.');
- 
-    res.send('normal http request');
+
+router.post('/entrar_sistema', function(req, res, next) {
+	// Recebendo o valor do post
+	POST = req.body;
+	POST.senha = control.Encrypt(POST.senha);
+
+	model.Login(POST).then(data => {
+		if(data.length > 0){
+			model.VerificarDeletado(data[0].id).then(dataDeletado => {
+				if(dataDeletado == ''){
+					req.session.usuario = {};
+					req.session.usuario.id = data[0].id;
+					req.session.usuario.hash_login = data[0].hash_login;
+					req.session.usuario.nivel = data[0].nivel;
+					res.send({result: 'redirect', url:'/sistema'});
+
+					/*Usuário deletado*/
+				}else{
+					res.send({result: 'banido', mensagem:'Usuário banido do aplicativo pela Administração',erro:'warn'});
+							// res.render('login/index', { erro: 'Usuário banido do aplicativo pela Administração', tipo_erro: 'login', usuario: req.session.usuario });
+						}
+					});
+
+		}else{
+			/*Não existe login ou senha no banco*/
+			res.send({result: 'loginerrado', mensagem:'Login ou senha incorreto(s).',erro:'warn'});
+		};
+	});
+});
+
+
+router.post('/recuperar/senha', function(req, res, next) {
+	var post = req.body;
+	var data_insert;
+	var nova_senha;
+	model.PesquisarEmail(post.email).then(idEmail => {
+		if(idEmail != ''){
+			nova_senha = Math.random().toString(36).substring(7);
+			data_insert = {id: idEmail[0].id, senha: nova_senha};
+			model.AlterarSenhaUsuarioPorId(data_insert).then(data_alterado_sucesso =>{
+				var html = "Olá, você está recebendo este e-mail pois pediu para recuperar sua senha"+
+				"<br>Sua nova senha na Eagle é: "+nova_senha+
+				"<br>Caso não pediu para recuperar a sua senha entre em contato com o Suporte pelo e-mail <a href='mailto:suporte@eagle.finance.com.br'>suporte@eagle.finance.com.br</a>"+
+				'<br><br>Não mostre seu login e senha para ninguém. A sua conta é responsabilidade sua.'+
+				'<br>Não responda esta mensagem, ela é enviada automaticamente.';
+				var text = "Olá, você está recebendo este e-mail pois pediu para recuperar sua senha"+
+				"<br>Sua nova senha na Eagle é: "+nova_senha+
+				"<br>Caso não pediu para recuperar a sua senha entre em contato com o Suporte pelo e-mail <a href='mailto:suporte@eagle.finance.com.br'>suporte@eagle.finance.com.br</a>"+
+				'<br><br>Não mostre seu login e senha para ninguém. A sua conta é responsabilidade sua.'+
+				'<br>Não responda esta mensagem, ela é enviada automaticamente.';
+				control.SendMail(post.email, 'Recuperação de Senha - Eagle Finance', html, text);				
+				res.json(data_alterado_sucesso);
+			});
+
+		}else{
+			res.json(['email_nao_cadastrado']);
+		}
+	});
 });
 
 
---
+++ app/model/apiModel.js
@@ -0,0 +1,103 @@
+ 'use strict';
+ var express = require('express');
+ var app = express();
+ var Helper = require('./model.js');
+ var helper = new Helper;
+
+ class ApiModel {
+
+ 	CadastrarUsuario(data) {
+ 		return new Promise(function(resolve, reject) {
+ 			helper.Query('SELECT id FROM usuarios WHERE nome_murer = ?', [data.nome_murer]).then(result => {
+ 				if (result.length <= 0) {
+ 					helper.Insert('usuarios', data).then(data => {
+ 						resolve(data);
+ 					});
+ 				} else {
+ 					resolve([]);
+ 				}
+ 			});
+ 		});
+ 	}
+
+
+ 	PesquisarEmail(email) {
+ 		return new Promise(function(resolve, reject) {
+ 			/*seleciono o id para ver se existe algum usuario com aquele email*/
+ 			helper.Query('SELECT id	FROM usuarios WHERE email = ? AND deletado = ? LIMIT 1',[email,0]).then(data => {
+ 				resolve(data);
+ 			});
+ 		});
+ 	}
+
+
+ 	AlterarSenhaUsuarioPorId(POST){
+ 		return new Promise(function(resolve, reject) {
+ 			POST.senha = helper.Encrypt(POST.senha);
+ 			helper.Update('usuarios', POST).then(data => {
+ 				resolve(data);
+ 			});
+ 		});
+ 	}
+
+ 	VerificarSenha(data) {
+ 		if (typeof data.senha != 'undefined') {
+ 			if (data.senha == data.senha_confirmar) {
+ 				delete data.senha_confirmar;
+ 				var senha = helper.Encrypt(data.senha);
+ 				data.senha = senha;
+ 				return data;
+ 			} else {
+ 				return [];
+ 			}
+ 		} else {
+ 			return [];
+ 		}
+ 	}
+
+
+ 	Login(POST) {
+ 		return new Promise(function(resolve, reject) {
+			// Adicione a query com scape(?) e os respectivos valores em um array simples
+			helper.Query('SELECT id, nivel FROM usuarios WHERE nome_murer = ? AND senha = ?', [POST.nome_murer, POST.senha]).then(data => {
+				if (typeof data != 'undefined' && data.length > 0) {
+					var hash_login = helper.Encrypt(Date());
+					data[0].hash_login = hash_login;
+					helper.Update('usuarios', {id: data[0].id, hash_login: hash_login}).then(data_up => {
+						resolve(data);
+					});
+				} else {
+					resolve(data);
+				}
+			});
+		});
+ 	}
+
+ 	Login(POST) {
+ 		return new Promise(function(resolve, reject) {
+			// Adicione a query com scape(?) e os respectivos valores em um array simples
+			helper.Query('SELECT id,nivel FROM usuarios WHERE login = ? AND senha = ?', [POST.login, POST.senha]).then(data => {
+				if (typeof data != 'undefined' && data.length > 0) {
+					var hash_login = helper.Encrypt(Date());
+					data[0].hash_login = hash_login;
+					helper.Update('usuarios', {id: data[0].id, hash_login: hash_login}).then(data_up => {
+						resolve(data);
+					});
+				} else {
+					resolve(data);
+				}
+			});
+		});
+ 	}
+
+
+ 	VerificarDeletado(id){
+ 		return new Promise(function(resolve, reject) {
+ 			helper.Query('SELECT id FROM usuarios WHERE id = ? AND deletado = ?', [id,1]).then(data => {
+ 				resolve(data);
+ 			});
+ 		});
+ 	}
+
+ }
+ module.exports = ApiModel;
