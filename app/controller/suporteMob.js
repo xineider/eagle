8e08b87eabd48ada4f63754e63cf4fe9f2a09cf0
Marcos Schneider <marcos.schneider13@hotmail.com>
2019-05-16 12:38:13 -0300

    teste umbler fora do ar

--- app/controller/suporteMob.js
+++ app/controller/suporteMob.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
 			model.GetPrimeiroAporte(req.session.usuario.id).then(data_primeiro_aporte=>{
 				data.aporte_primeiro = data_primeiro_aporte;
 				data.link_sistema = '/mobsmart';
-				console.log('===================== DATA USUARIO ====================');
+				console.log('===================== DATA USUARIO ===================');
 				console.log(data);
 				console.log('=======================================================');
 				res.render(req.isAjaxRequest() == true ? 'api' : 'montadorMobile', {html: 'suporte/suporte', data: data, usuario: req.session.usuario});
