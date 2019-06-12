47e2e82864166add44d26d10224f3790773c7b0e
Marcos Schneider <marcos.schneider13@hotmail.com>
2019-06-12 19:38:11 -0300

    alterado contrato

--- app/controller/index.js
+++ app/controller/index.js
@@ -24,8 +24,8 @@ router.get('/', function(req, res, next) {
 					console.log(data_confirmacao_contrato);
 					console.log('cccccccccccccccccccccccccccccccccccccccccccccccccccc');
 					if(data_confirmacao_contrato != ''){
-						// res.render(req.isAjaxRequest() == true ? 'api' : 'montador', {html: 'inicio/index', data: data, usuario: req.session.usuario});
-						res.render(req.isAjaxRequest() == true ? 'api' : 'montador', {html: 'inicio/contrato', data: data, usuario: req.session.usuario});
+						res.render(req.isAjaxRequest() == true ? 'api' : 'montador', {html: 'inicio/index', data: data, usuario: req.session.usuario});
+						// res.render(req.isAjaxRequest() == true ? 'api' : 'montador', {html: 'inicio/contrato', data: data, usuario: req.session.usuario});
 					}else{
 
 						console.log('--------------- DATA  INICIO------------------');
